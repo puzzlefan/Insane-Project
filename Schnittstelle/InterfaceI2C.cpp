aa6c018b27960633e065a51d3954a03ca0199904
puzzlefan <bekep00@gmail.com>
2017-04-09 10:53:52 +0200

    ...

--- Schnittstelle/InterfaceI2C.cpp
+++ Schnittstelle/InterfaceI2C.cpp
@@ -11,35 +11,6 @@ void Schnittstelle::InterfaceSetUp()
 	mcp23017Setup(300, 0x22);
 	mcp23017Setup(400, 0x24);
 
-	//Einstellen der verschiedenen Pins auf In- und Output
-	pinMode(rvUltraschallsensorEcho1, INPUT);
-	pinMode(rvUltraschallsensorEcho2, INPUT);
-	pinMode(rvUltraschallsensorEcho3, INPUT);
-	pinMode(lvUltraschallsensorEcho1, INPUT);
-	pinMode(lvUltraschallsensorEcho2, INPUT);
-	pinMode(lvUltraschallsensorEcho3, INPUT);
-
-	pinMode(rvUltraschallsensorTrigger1, OUTPUT);
-	pinMode(rvUltraschallsensorTrigger2, OUTPUT);
-	pinMode(rvUltraschallsensorTrigger3, OUTPUT);
-	pinMode(lvUltraschallsensorTrigger1, OUTPUT);
-	pinMode(lvUltraschallsensorTrigger2, OUTPUT);
-	pinMode(lvUltraschallsensorTrigger3, OUTPUT);
-
-	pinMode(rhUltraschallsensorEcho1, INPUT);
-	pinMode(rhUltraschallsensorEcho2, INPUT);
-	pinMode(rhUltraschallsensorEcho3, INPUT);
-	pinMode(lhUltraschallsensorEcho1, INPUT);
-	pinMode(lhUltraschallsensorEcho2, INPUT);
-	pinMode(lhUltraschallsensorEcho3, INPUT);
-
-	pinMode(rhUltraschallsensorTrigger1, OUTPUT);
-	pinMode(rhUltraschallsensorTrigger2, OUTPUT);
-	pinMode(rhUltraschallsensorTrigger3, OUTPUT);
-	pinMode(lhUltraschallsensorTrigger1, OUTPUT);
-	pinMode(lhUltraschallsensorTrigger2, OUTPUT);
-	pinMode(lhUltraschallsensorTrigger3, OUTPUT);
-
 	pinMode(Parken, INPUT);
 	pinMode(fahrtModiNormalesFahren, INPUT);
 	pinMode(fahrtModiDrehen, INPUT);
