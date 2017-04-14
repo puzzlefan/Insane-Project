2827116429b8c87cd957c76b4096398116797005
puzzlefan <bekep00@gmail.com>
2017-04-14 20:47:08 +0200

    5

--- Displays/lcm1602.h
+++ Displays/lcm1602.h
@@ -1,3 +1,22 @@
-#ifndef 
+#ifndef lcm1602_H
+#define lcm1602_H
 
-#endif // !
+#include <string>
+
+class LCM
+{
+private:
+	int LCDAddr = 0x27;
+	int BLEN = 1;
+	int fd;
+
+public:
+	LCM();
+	void write_word(int data);
+	void send_command(int comm);
+	void send_data(int data);
+	void clear();
+	void write(int x, int y, const std::string &data);
+};
+
+#endif // !lcm1602_H
