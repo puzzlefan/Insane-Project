a38d6f60ae45daae4380d5d1044024ba6cf15278
puzzlefan <bekep00@gmail.com>
2017-04-14 20:49:15 +0200

    6

--- Displays/lcm1602.cpp
+++ Displays/lcm1602.cpp
@@ -2,12 +2,26 @@
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 #include <string.h>
+#include <lcm1602.h>
 
-int LCDAddr = 0x27;
-int BLEN = 1;
-int fd;
+LCM::LCM()
+{
+	fd = wiringPiI2CSetup(LCDAddr);
 
-void write_word(int data) {
+	send_command(0x33); // Must initialize to 8-line mode at first
+	delay(5);
+	send_command(0x32); // Then initialize to 4-line mode
+	delay(5);
+	send_command(0x28); // 2 Lines & 5*7 dots
+	delay(5);
+	send_command(0x0C); // Enable display without cursor
+	delay(5);
+	send_command(0x01); // Clear Screen
+	wiringPiI2CWrite(fd, 0x08);
+}
+
+void LCM::write_word(int data) 
+{
 	int temp = data;
 	if (BLEN == 1)
 		temp |= 0x08;
@@ -16,7 +30,8 @@ void write_word(int data) {
 	wiringPiI2CWrite(fd, temp);
 }
 
-void send_command(int comm) {
+void LCM::send_command(int comm) 
+{
 	int buf;
 	// Send bit7-4 firstly
 	buf = comm & 0xF0;
@@ -35,7 +50,8 @@ void send_command(int comm) {
 	write_word(buf);
 }
 
-void send_data(int data) {
+void LCM::send_data(int data) 
+{
 	int buf;
 	// Send bit7-4 firstly
 	buf = data & 0xF0;
@@ -54,26 +70,15 @@ void send_data(int data) {
 	write_word(buf);
 }
 
-void init() {
-	send_command(0x33); // Must initialize to 8-line mode at first
-	delay(5);
-	send_command(0x32); // Then initialize to 4-line mode
-	delay(5);
-	send_command(0x28); // 2 Lines & 5*7 dots
-	delay(5);
-	send_command(0x0C); // Enable display without cursor
-	delay(5);
-	send_command(0x01); // Clear Screen
-	wiringPiI2CWrite(fd, 0x08);
-}
-
-void clear() {
+void LCM::clear() 
+{
 	send_command(0x01); //clear Screen
 }
 
-void write(int x, int y, char data[]) {
-	int addr, i;
-	int tmp;
+void LCM::write(int x, int y, const std::string &data) 
+{
+	int addr;
+
 	if (x < 0) x = 0;
 	if (x > 15) x = 15;
 	if (y < 0) y = 0;
@@ -83,8 +88,8 @@ void write(int x, int y, char data[]) {
 	addr = 0x80 + 0x40 * y + x;
 	send_command(addr);
 
-	tmp = strlen(data);
-	for (i = 0; i < tmp; i++) {
-		send_data(data[i]);
+	const int len = data.length();
+	for (int i = 0; i < len; i++) {
+		send_data(data.c_str()[i]);
 	}
 }
