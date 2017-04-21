d580e1ad233989a0cc76810a790f1ccb5faca458
kloontim <timon.gronotte@hotmail.de>
2017-04-21 13:28:46 +0200

    Update libSonar.h

--- Ultrasonic/libSonar.h
+++ Ultrasonic/libSonar.h
@@ -1,22 +1,35 @@
 #ifndef DEF_SONAR
 #define DEF_SONAR
-
+#define _USE_MATH_DEFINES
+#include "math.h"
 class Sonar
 {
-  public:
-    Sonar();
-    void init(int trigger, int echo);
-    double distance(int timeout);
+public:
+	Sonar();//Constructr does nothing
+	void init(int trigger, int echo);//init for simple Sensors
+	void init(int trigger, int echo, int height);//init for simple straigth Ultrasonic sensors
+	void init(int trigger, int echo, int height, int angle);//init for angledS Sensors
+	double distance(int timeout);// mesurment function
+	double calcDistance(int timeout);//calculated distance for angled sensors 
+	double calcHeigth(int timeout);//calculated heigth for angled sensors
+	double getMountigHigth() { return height; }
+	double getmountingAngle() { return angle; }
 
-  private:
-    void recordPulseLength();
-    int trigger;
-    int echo;
-    volatile long startTimeUsec;
-    volatile long endTimeUsec;
-    double distanceMeters;
-    long travelTimeUsec;
-    long now;
+private:
+	//Functions
+	void recordPulseLength();
+	//Init values
+	int trigger;//-pin
+	int echo;//reciver pin
+	int height = -1;//the higth in which the speaker and mic are fixed at
+	int angle = -1;//the angle, from the mesurement direction to a straigth line to the ground
+	//Calculation values
+	volatile long startTimeUsec;
+	volatile long endTimeUsec;
+	double distanceMeters;
+	long travelTimeUsec;
+	long now;
+	const double DegreeToRad = (2 * M_PI) / 360;
 };
 
 #endif
